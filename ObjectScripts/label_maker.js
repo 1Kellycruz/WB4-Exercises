68a7d4181420ad48adfa85bbaf7e416afd9ab855
Kelly Cruz <Kells73002@gmail.com>
2023-05-08 11:42:38 -0400

    Label Maker

---
+++ ObjectScripts/label_maker.js
@@ -0,0 +1,16 @@
+const myInfo = {
+    name: "Kelly Cruz",
+    address: "6607 Horseshoe Bend",
+    city: "Orlando",
+    state: "Florida",
+    zip: "32822"
+    
+};
+
+printContact(myInfo);
+
+function printContact(myInfo) {
+    console.log(myInfo.name);
+    console.log(myInfo.address);
+    console.log(`${ myInfo.city }, ${ myInfo.state }, ${ myInfo.zip }`);
+}
