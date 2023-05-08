cd3961d05cec6e2b0fb345e1db69151b1db6ea4c
Kelly Cruz <Kells73002@gmail.com>
2023-05-08 15:27:32 -0400

    My Family

---
+++ ArrayScripts/my_family.js
@@ -0,0 +1,9 @@
+let names = ["Kevin", "Kemely", "Kenny", "Kelly"];
+let oldest = names[0];
+let middle = names[1];
+let secmiddle = names[2];
+let youngest = names[3];
+
+for (let i = 0; i < 4; i++) {
+    console.log(names[i]);
+}
