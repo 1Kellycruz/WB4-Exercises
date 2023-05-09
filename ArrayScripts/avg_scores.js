b9288378406b0171f164164b065da4aeb41e86c5
Kelly Cruz <Kells73002@gmail.com>
2023-05-09 09:22:04 -0400

    scores

--- ArrayScripts/avg_scores.js
+++ ArrayScripts/avg_scores.js
@@ -1,2 +1,30 @@
 let myScores = [92, 98, 84, 76, 89, 99, 100];
-let yourScores = [82, 98, 94, 88, 92, 100, 100]
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+
+
+
+
+
+
+
+
+
+function getAverage(scores) {
+    let sum = 0
+    for (let i = 0; i < scores.length; i++) {
+        sum += scores[i]
+    }
+
+    // divide sum by length of average
+    const average = sum / scores.length;
+    return average;
+}
+
+const myAverage = getAverage(myScores)
+console.log(myAverage)
+
+const yourAverage = getAverage(yourScores)
+console.log(yourAverage)
+
+
+
